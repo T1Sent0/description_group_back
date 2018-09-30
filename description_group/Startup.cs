64b60f06aa9caae90e343d5d76e55b35e2205763
Aleksey <foregestore@gmail.com>
2018-09-30 19:14:06 +0300

    added swager

--- description_group/Startup.cs
+++ description_group/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace description_group
 {
@@ -23,6 +24,21 @@ namespace description_group
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "API Description",
+                    Description = "ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Aleksey Gaydash",
+                        Email = "foregestore@gmail.com",
+                    }
+                });
+            });
             services.AddMvc();
         }
 
@@ -33,7 +49,12 @@ namespace description_group
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Descrition v1.0.0");
+            });
             app.UseMvc();
         }
     }
